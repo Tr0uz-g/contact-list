86e35b52e97280105523e5e3ea77f45efe09a2ea
Pablo Torres <pabltorrs958@gmail.com>
2023-08-15 07:35:29 -0500

    crear y eliminar contactos

--- vida.js
+++
@@ -1,9 +0,0 @@
-
-let contactosDatos = ["Pablo Torre", "Sofia Gutierres", "Alberto Rodriguez"]
-
-contactosDatos.push("Jose Suarez")
-
-let contactoEliminado = contactosDatos.shift("Luis Lopez")
-
-console.log(contactosDatos)
-console.log(`${contactoEliminado}: este contacto se elimino con el metodo shift (__elimina_el_primer_elemento__) y se devolvio a la fucion contactoEliminado`)
